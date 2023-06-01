a5e90c2d50be3bbec2569c83eec6e787e7311009
Sowgenya <125674476+Sowgenya@users.noreply.github.com>
2023-06-01 19:16:53 +0530

    Virtual Reality Industrial Safety Training

---
+++ industry/Assets/JMO Assets/WarFX/Scripts/flicklight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class flicklight : MonoBehaviour
+{
+    Light testLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        testLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            testLight.enabled = !testLight.enabled;
+        }
+    } }
+
+   
